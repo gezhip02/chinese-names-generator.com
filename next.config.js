9bf3735ab7aa336aaf862e82f62421b20cb73920
gezhip <gezhip@gmail.com>
2025-02-21 07:14:29 +0800

    init

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     env: {
-      DEEPSEEK_API_KEY: process.env.DEEPSEEK_API_KEY,
+      API_URL: process.env.API_URL,
+      API_KEY: process.env.API_KEY,
     },
   }
   
